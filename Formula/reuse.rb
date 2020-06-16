c984078eae6f0911faab8be2b384b3a5541860f5
Jesper Oskarsson <jesosk@gmail.com>
2020-06-16 13:30:18 +0200

    No need to build standard library anymore

--- Formula/reuse.rb
+++ Formula/reuse.rb
@@ -1,9 +1,9 @@
 
 class Reuse < Formula
     desc "Compiler for the Reuse programming language"
-    homepage ""
+    homepage "https://github.com/redien/reuse-lang"
     head "https://github.com/redien/reuse-lang.git"
-  
+
     depends_on "ocaml"
     depends_on "coreutils"
 
@@ -11,9 +11,8 @@ class Reuse < Formula
         libexec.install Dir["*"]
         bin.write_exec_script (libexec/"reusec")
         system "bash", (libexec/"build.sh")
-        system "bash", (libexec/"standard-library/build.sh")
     end
-  
+
     test do
         File.open('test.reuse', 'w') { |file| file.write('(def main (stdin) stdin)') }
         system bin/"reusec", "--output", "test", "--executable", "test.reuse"
